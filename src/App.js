a6969751db5581c8f70592605ac51cdfe4aed0d1
ICEDBANK <j.e.rice4101@gmail.com>
2024-04-26 23:22:09 -0400

    Automated commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
-
+import { useState , useEffect } from "react";
 
 function App() {
+
+  const [count, setCount] = useState(0);
+  
+
   return (
 
     <>
 
-       <h1>Understanding useEffect</h1>
-       <p>
-          
-       </p>
+      <h1>Understanding useEffect</h1>
+      <h2> {count} </h2>
+      <button onClick={() => setCount( count + 1 )}>Increment</button>
 
     </>
 
